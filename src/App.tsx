34e60d80ae2d91960ec8cd86cea99f9b5b36b125
riolly <alberto.riolly@gmail.com>
2023-08-11 18:48:21 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-const Link = (props: JSX.IntrinsicElements['a']) => (
-  <a
-    className="text-pink-500 underline hover:no-underline dark:text-pink-400"
-    {...props}
-  />
-);
-
 export default function App() {
   return (
-    <div className="mx-auto my-8 mt-10 w-8/12 rounded border border-gray-200 p-4 shadow-md dark:border-neutral-600 dark:bg-neutral-800 dark:shadow-none">
-      <h1 className="mb-4 text-4xl">Welcome</h1>
-      <p className="my-4">
-        <em>Minimal, fast, sensible defaults.</em>
-      </p>
-      <p className="my-4">
-        Using <Link href="https://vitejs.dev/">Vite</Link>,{' '}
-        <Link href="https://reactjs.org/">React</Link>,{' '}
-        <Link href="https://www.typescriptlang.org/">TypeScript</Link> and{' '}
-        <Link href="https://tailwindcss.com/">Tailwind</Link>.
-      </p>
-      <p className="my-4">
-        Change{' '}
-        <code className="border-1 2py-1 rounded border border-pink-500 bg-neutral-100 px-1 font-mono text-pink-500 dark:border-pink-400 dark:bg-neutral-700 dark:text-pink-400">
-          src/App.tsx
-        </code>{' '}
-        for live updates.
-      </p>
+    <div className="h-screen bg-gradient-to-br from-slate-900 to-violet-950">
+      <div className="mx-auto max-w-screen-md pt-12">
+        <h1 className="text-center text-4xl text-primary-foreground">
+          Simple Logo Maker
+        </h1>
+        {/* <div className="h-40 w-full bg-blue-800"></div> */}
+      </div>
     </div>
   );
 }
