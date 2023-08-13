f1249431ab4e81dc4d15c97f8af6bcbbbbcd447f
riolly <alberto.riolly@gmail.com>
2023-08-13 23:51:17 +0700

    chore: generateId

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { customAlphabet } from 'nanoid'
+
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function generateId() {
+  const nanoId = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789', 16)
+  return nanoId()
 }
